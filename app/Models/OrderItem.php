0ae32c1489376945d6770d076d2e1d87f113e44b
Juzzrav <juzzleravena@gmail.com>
2025-12-19 22:49:23 +0800

    feat(model): add order item model with toppings relation

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'order_id','pizza_id','type','name_snapshot','unit_price_minor','qty','line_total_minor'
+    ];
+
+    public function toppings()
+    {
+        return $this->hasMany(OrderItemTopping::class);
+    }
 }
