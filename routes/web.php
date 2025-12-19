62e972fc5d91b12b2537ac1dee73c0007b07820f
Juzzrav <juzzleravena@gmail.com>
2025-12-19 22:50:20 +0800

    feat(routes): register checkout and order routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\OrderController;
+
 
 Route::get('/', [MenuController::class, 'index'])->name('menu');
 
@@ -10,3 +13,7 @@ Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
 Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
 Route::post('/cart/remove/{key}', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
+Route::get('/checkout', [CheckoutController::class, 'show'])->name('checkout.show');
+Route::post('/checkout', [CheckoutController::class, 'process'])->name('checkout.process');
+
+Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
